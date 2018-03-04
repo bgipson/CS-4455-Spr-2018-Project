5fdc05fafa4665e602dd9d82c8913fa36abc27b5
ambuzv <ambuzvimal@gmail.com>
2018-03-03 22:54:18 -0500

    Collectible Script

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour {
+
+    public GameObject pickle;
+    public static bool pickle_activated;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.transform.tag == "Pickle")
+        {
+            pickle_activated = true;
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
