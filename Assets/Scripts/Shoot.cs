d001defdc09f8c5d21830275dddd5817ef70ac3f
AnnaGreene <annagrn@gatech.edu>
2018-03-15 17:13:28 -0400

    high jump, squish and shield

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -9,33 +9,66 @@ public class Shoot : MonoBehaviour
     public Rigidbody bullet;
     public float power;
     public PowerUpManager powerUpManager;
+    Animator animator;
     //float time = 0f;
    
     void Start()
     {
         power = 30f;
+        animator = GetComponent<Animator>();
     }
-
     
     void FixedUpdate()
     {
         
-            if (Input.GetButton("Fire1") && Collectibles.pickle_acquired == true && powerUpManager.pickle_enabled == true)
+        if (Input.GetButton("Fire1")) //button pressed
+        {
+            if (Collectibles.pickle_acquired && powerUpManager.pickle_enabled) //pickle selected
             {
-
                 Debug.Log("Pressed");
                 if (power < 150f)
                 {
-
                     power += 2f;
-
                 }
-                
             }
-            if (Input.GetButtonUp("Fire1") && Collectibles.pickle_acquired == true && powerUpManager.pickle_enabled == true)
+            else if (Collectibles.cheese_acquired && powerUpManager.cheese_enabled) //Collectibles.cheese_acquired && 
+            {
+                //super jump
+                animator.SetBool("HighJump", true);
+            }
+            else if (Collectibles.tomato_acquired && powerUpManager.tomato_enabled) //Collectibles.tomato_acquired &&
+            {
+                //squish enabled
+                animator.SetBool("Squished", true);
+            }
+            else if (Collectibles.lettuce_acquired && powerUpManager.lettuce_enabled)
+            {
+                //shield enabled
+            }
+
+        }
+
+        if (Input.GetButtonUp("Fire1")) //button let go
+        {
+            if (Collectibles.pickle_acquired && powerUpManager.pickle_enabled) //pickle selected
             {
                 Shooting();
             }
+            //else if (Collectibles.cheese_acquired && powerUpManager.cheese_enabled)
+            //{
+            //    //super jump
+            //}
+            else if (Collectibles.tomato_acquired && powerUpManager.tomato_enabled) //Collectibles.tomato_acquired && 
+            {
+                //squish disabled
+                animator.SetBool("Squished", false);
+            }
+            else if (Collectibles.lettuce_acquired && powerUpManager.lettuce_enabled)
+            {
+                //shield disabled
+            }
+        }
+        
         
     }
 
