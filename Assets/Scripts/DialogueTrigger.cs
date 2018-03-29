bb50d67d6cbc5d791bd074f12be96ed155149a43
cmsalmon <coreymsalmon@gmail.com>
2018-03-29 15:51:43 -0400

    Dialogue function is completed
    Need to think of good text still though

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+    //https://www.youtube.com/watch?v=_nRzoTzeyxU
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Burgie")
+        {
+            TriggerDialogue();
+        }
+    }
+}
