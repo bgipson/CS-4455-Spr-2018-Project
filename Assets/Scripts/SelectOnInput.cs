52909489f0ba4cef940bf607e0ca2c1ea90fc3d6
AnnaGreene <annagrn@gatech.edu>
2018-04-06 18:35:55 -0400

    basic start menu
    you can now start the tutorial (currently hard coded) or exit

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.LogError(Input.GetAxisRaw("Vertical"));
+        //if (Input.GetAxisRaw("Vertical") !=0)
+        //{
+        //    Debug.LogError("vertical");
+        //}
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
