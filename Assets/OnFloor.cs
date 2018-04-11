73c8214c37aa1565e34f67790245eff1f080fb7e
avimal3 <avimal@gatech.edu>
2018-04-11 02:00:55 -0400

    AI
    Mouse AI (Player Following) with cube placeholders.

---
+++ Assets/OnFloor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnFloor : MonoBehaviour {
+
+    public bool isPlayeronfloor;
+    void Start()
+    {
+        isPlayeronfloor = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            isPlayeronfloor = true;
+    }
+}
