c91ad5f430a1fdd75aacd618eb2f4ccbabf3e055
AnnaGreene <annagrn@gatech.edu>
2018-04-13 13:31:52 -0400

    added credits

---
+++ Assets/EndCredits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits : MonoBehaviour {
+
+	public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
