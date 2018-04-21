b3fa4fbc8aa5e5a0902740ab037d0bcbdb4be0a2
avimal3 <avimal@gatech.edu>
2018-04-20 22:21:07 -0400

    Fork Particle Effect

--- Assets/Scripts/ForkAI.cs
+++ Assets/Scripts/ForkAI.cs
@@ -8,24 +8,37 @@ public class ForkAI : MonoBehaviour {
     public Transform target;
     public float distanceToTarget;
     public float turnSpeed = 10f;
-	// Use this for initialization
-	void Start () {
-        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    public ParticleSystem Circle;
+   // public GameObject Triangle;
+    //public Material[] mats;
+    //var main = Triangle.main;
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.01f);
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (target == null)
+        {
+            Circle.startColor = new Color(0f, 0.5f, 1f);
+//Triangle.GetComponent<Renderer>().material = mats[1];
             return;
+        }
 
-       
+        Circle.startColor = new Color(1f,0f,0f);
+        //Triangle.startColor = new Color(1f, 0f, 0f);
+        
         Vector3 targetDir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(targetDir);
         Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles;
-        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f); 
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+       // Triangle.GetComponent<Renderer>().material = mats[0];
+        //Triangle.startRotation = gameObject.transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+        
 
 
-       
     }
 
     void UpdateTarget()
