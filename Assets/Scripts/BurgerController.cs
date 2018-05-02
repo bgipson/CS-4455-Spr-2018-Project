7579c0416d8cd1884a8e0540c428ec6ac27264bf
avimal3 <avimal@gatech.edu>
2018-05-02 02:39:59 -0400

    Controller Input

--- Assets/Scripts/BurgerController.cs
+++ Assets/Scripts/BurgerController.cs
@@ -11,7 +11,8 @@ using UnityEngine.SceneManagement;
 /// 4. Squished (MOVING)
 /// 5. Hurt Animation
 /// </summary>
-public class BurgerController : MonoBehaviour {
+public class BurgerController : MonoBehaviour
+{
     public DoAction doAction;
     public PowerUpManager powerUpManager;
     Animator animator;
@@ -21,13 +22,15 @@ public class BurgerController : MonoBehaviour {
     public GroundCheck groundCheck;
     Collectibles manager;
     public int health = 3;
+    //Vector3 dir;
 
     public bool joystick = false;
     float airVelocity = 0f;
 
     public bool invincible = false;
 
-    void Start () {
+    void Start()
+    {
 
         doAction = GetComponent<DoAction>();
         animator = GetComponent<Animator>();
@@ -39,9 +42,10 @@ public class BurgerController : MonoBehaviour {
         Collectibles.tomato_acquired = false;
         Collectibles.lettuce_acquired = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         readKeyboard();
         readJoystick();
         checkGround();
@@ -51,16 +55,19 @@ public class BurgerController : MonoBehaviour {
     public GameObject cheese;
     public GameObject tomato;
     public GameObject lettuce;
-    public void layerCheck() {
+    public void layerCheck()
+    {
         cheese.SetActive(manager.getCheese());
         tomato.SetActive(manager.getTomato());
         lettuce.SetActive(manager.getLettuce());
-        if (!FindObjectOfType<PowerUpManager>().tomato_enabled) {
+        if (!FindObjectOfType<PowerUpManager>().tomato_enabled)
+        {
             animator.SetBool("Squished", false);
         }
     }
     //Checks if there is solid ground under
-    void checkGround() {
+    void checkGround()
+    {
         Ray ray = new Ray(raycastPoint.transform.position, Vector3.down);
         RaycastHit hit;
         //bool grounded = Physics.Raycast(ray, out hit, 2f, 1 << 8);
@@ -70,40 +77,52 @@ public class BurgerController : MonoBehaviour {
         animator.applyRootMotion = true;
     }
 
-    void readKeyboard() {
-        if (Input.GetKey(KeyCode.UpArrow)) {
+    void readKeyboard()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
             animator.SetBool("Jump", true);
             joystick = false;
         }
-        if (!joystick) {
-            if (animator.GetBool("HighJump") && !animator.GetBool("Grounded")) {
+        if (!joystick)
+        {
+            if (animator.GetBool("HighJump") && !animator.GetBool("Grounded"))
+            {
                 //float airAngelDiff = Mathf.Abs(Vector3.Angle(dir, airForward));
                 //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(airForward), 0.1f);
                 airVelocity = Mathf.Lerp(airVelocity, 0.9f, 0.625f * Time.deltaTime);
                 transform.position += transform.forward * ((62.5f * Time.deltaTime) * airVelocity);
             }
-            if (animator.GetBool("Grounded")) {
+            if (animator.GetBool("Grounded"))
+            {
                 airVelocity = 0;
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
             animator.SetBool("Jump", false);
             joystick = false;
         }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
             transform.Rotate(new Vector3(0, -5f, 0));
             //rig.angularVelocity = new Vector3(rig.angularVelocity.x, -4, 0);
             animator.SetBool("Turning", true);
             joystick = false;
-        } else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
             rig.angularVelocity = Vector3.zero;
             animator.SetBool("Turning", false);
-        } else {
+        }
+        else
+        {
             rig.angularVelocity = Vector3.zero;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.Rotate(new Vector3(0, 5f, 0));
             //rig.angularVelocity = new Vector3(rig.angularVelocity.x, 4, 0);
             joystick = false;
@@ -111,41 +130,68 @@ public class BurgerController : MonoBehaviour {
             //transform.Rotate(0, 4, 0);
         }
 
-        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
             rig.angularVelocity = Vector3.zero;
             animator.SetBool("Turning", false);
         }
 
-        animator.SetBool("Fast", (Input.GetKey(KeyCode.LeftShift)|| Input.GetKeyDown("joystick button 0")));
-        
-        
+        animator.SetBool("Fast", (Input.GetKey(KeyCode.LeftShift) || Input.GetKeyDown("joystick button 10")));
+
+
     }
 
-    
-    void readJoystick() {
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.85f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.85f) {
-            Vector3 dir = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")));
 
-            
-            
-            if (animator.GetBool("HighJump") && !animator.GetBool("Grounded")) {
+    void readJoystick()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float ang;
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.85f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.85f)
+        {
+            Vector3 dir = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")));
+            ang = Vector3.SignedAngle(new Vector3(0, 0, 1f), transform.forward, Vector3.up);
+            dir = Quaternion.AngleAxis(ang, Vector3.up) * dir;
+
+            if (animator.GetBool("HighJump") && !animator.GetBool("Grounded"))
+            {
                 float airAngelDiff = Mathf.Abs(Vector3.Angle(dir, airForward));
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(airForward), 0.1f);
                 airVelocity = Mathf.Lerp(airVelocity, 0.9f, 0.01f);
                 transform.position += airForward * airVelocity;
-            } else {
+            }
+            else
+            {
                 dir = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")));
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.1f);
+                ang = Vector3.SignedAngle(new Vector3(0, 0, 1f), transform.forward, Vector3.up);
+                dir = Quaternion.AngleAxis(ang, Vector3.up) * dir;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.05f);
                 animator.SetBool("Jump", true);
             }
             joystick = true;
-        } else if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.3f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.8f) {
+        }
+        else if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.3f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.8f)
+        {
             Vector3 dir = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")));
+            ang = Vector3.SignedAngle(new Vector3(0, 0, 1f), transform.forward, Vector3.up);
+            Debug.Log(ang);
+            dir = Quaternion.AngleAxis(ang, Vector3.up) * dir;
+
+
+
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.1f);
+            Debug.Log(transform.forward);
+            Debug.Log("transform");
+            Debug.Log(Quaternion.LookRotation(dir).eulerAngles);
+            Debug.Log("dir");
             animator.SetBool("Turning", true);
             joystick = true;
-        } else if (joystick) {
-            if (animator.GetBool("HighJump") && !animator.GetBool("Grounded")) {
+        }
+        else if (joystick)
+        {
+            if (animator.GetBool("HighJump") && !animator.GetBool("Grounded"))
+            {
                 airVelocity = Mathf.Clamp(airVelocity - 0.2f, 0, airVelocity);
                 transform.position += airForward * airVelocity;
             }
@@ -163,58 +209,80 @@ public class BurgerController : MonoBehaviour {
     }
 
     Vector3 airForward;
-    public void jump() {
+    public void jump()
+    {
         //rig.isKinematic = true;
         Vector3 dir;
-        if (joystick && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.3f && Mathf.Abs(Input.GetAxis("Vertical")) < 0.3f) {
+        if (joystick && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.3f && Mathf.Abs(Input.GetAxis("Vertical")) < 0.3f)
+        {
             dir = transform.forward;
-        } else {
+        }
+        else
+        {
             dir = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")));
         }
         rig.velocity = new Vector3(30, 45, rig.velocity.z);
         airForward = dir;
         //transform.position = transform.position + (transform.forward * 56);
-        
+
     }
 
-    public void endJump() {
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.black;
+    //    Gizmos.DrawLine(transform.position, dir);
+
+    //}
+
+    public void endJump()
+    {
         rig.isKinematic = false;
     }
 
-    public void playParticle() {
+    public void playParticle()
+    {
         ParticleSystem part = GetComponentInChildren<ParticleSystem>();
-        if (part) {
+        if (part)
+        {
             part.Play();
         }
     }
 
-    public void playAudio(AudioClip clip) {
+    public void playAudio(AudioClip clip)
+    {
         AudioManager audioManager = FindObjectOfType<AudioManager>();
-        if (audioManager) {
+        if (audioManager)
+        {
             audioManager.playAudio(clip);
         }
     }
 
 
-    void OnTriggerStay(Collider other) {
+    void OnTriggerStay(Collider other)
+    {
 
 
 
-        if (!invincible && other.gameObject.tag == "Enemy" && doAction.shield_on == false) {
+        if (!invincible && other.gameObject.tag == "Enemy" && doAction.shield_on == false)
+        {
             health -= 1;
-            if (health <= 0) {
+            if (health <= 0)
+            {
                 animator.SetBool("Dead", true);
                 invincible = true;
                 ScreenTransition transition = FindObjectOfType<ScreenTransition>();
-                if (transition) {
+                if (transition)
+                {
                     transition.fadeInToLevel(SceneManager.GetActiveScene().buildIndex);
                 }
                 rig.constraints = RigidbodyConstraints.FreezeAll;
-            } else {
+            }
+            else
+            {
                 animator.SetBool("Damage", true);
                 StartCoroutine(hurtBufferPeriod(1.3f));
             }
-            
+
         }
 
         if (!invincible && other.gameObject.tag == "Fire" && doAction.shield_on == false)
@@ -241,14 +309,15 @@ public class BurgerController : MonoBehaviour {
         }
     }
 
-    
+
 
     //Buffer period between getting hurt when you're invincible.
-    IEnumerator hurtBufferPeriod(float waitTime) {
+    IEnumerator hurtBufferPeriod(float waitTime)
+    {
         invincible = true;
         yield return new WaitForSeconds(waitTime);
         invincible = false;
     }
 
-    
+
 }
