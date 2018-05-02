42fb94f55f4073c96f92e1512e0754ddbdf2419e
AnnaGreene <annagrn@gatech.edu>
2018-05-02 00:23:38 -0400

    controls

---
+++ Assets/Scripts/controllerImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controllerImage : MonoBehaviour {
+
+    public Image keyboard;
+    public Image controller;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetJoystickNames().Length == 1)
+        {
+            keyboard.gameObject.SetActive(false);
+            controller.gameObject.SetActive(true);
+        }
+        else
+        {
+            keyboard.gameObject.SetActive(true);
+            controller.gameObject.SetActive(false);
+        }
+
+    }
+}
